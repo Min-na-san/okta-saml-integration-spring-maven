dad55662e57986984d6fef79938eb3e029739368
Min-na-san <58392061+Min-na-san@users.noreply.github.com>
2023-11-25 12:44:46 +0800

    Add files via upload
    working spring Saml okta

---
+++ saml-integrations/src/test/java/com/org/okta/samlintegrations/SamlIntegrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.okta.samlintegrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamlIntegrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
